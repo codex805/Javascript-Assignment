239fed28ad265d8c36bf8097de537be12a639e04
jihad805 <bjihad805@gmail.com>
2025-06-24 16:30:44 +0600

    selected and if

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const input = document.querySelector("#todoInput");
 const adddBtn = document.querySelector("#addBtn");
 const list = document.querySelector("#todoList");
 
+// add TO-Do
+
+adddBtn.addEventListener("click", ()=>{
+    const text = input.value.trim();
+    if(text !== ""){
+       addTodoItem(text);
+       input.value = "";
+    }
+})
